f7161f1202316321875d0ac59957b880fef0b6b6
Jonathan Taylor <jonyonson@icloud.com>
2019-04-02 15:52:14 -0500

    completed strech problem

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -75,4 +75,7 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  return cb([...new Set(array)]);
 }
+
+removeDuplicates([1, 2, 2, 3], noDuplicates => console.log(noDuplicates)); // 1, 2, 3
